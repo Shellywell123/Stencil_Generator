a86a950fe946801331a1c428237f6a4b8c1c3ddf
Shellywell123 <benjamin@shellswell.com>
2020-11-28 16:48:17 +0000

    1s commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Stencil Generator
+python3 program that coverts an image file into 3 stencils, by cranking up the contrast and isolating different tones.
+
+<p float="middle">
+  <img src="md_example.png" width="400" />
+</p>
---
+++ run.py
@@ -0,0 +1,71 @@
+
+image_name = "test_image.jpg"
+
+print('making stencils from {}'.format(image_name))
+#increase contrast
+from PIL import Image,ImageEnhance,ImageOps
+img=Image.open(image_name)
+img_contr_obj=ImageEnhance.Contrast(img)
+factor=10000
+e_img=img_contr_obj.enhance(factor)
+e_img = ImageOps.grayscale(e_img)
+e_img.save('combined_stencils.png')
+
+print('contranst config = {}'.format(factor))
+
+# check num of cols
+from collections import Counter
+colors = Counter(e_img.getdata())
+print(len(colors),'different shades')                  # number of unique colors 
+
+# make 3 stencils
+hi_lim = 200
+lo_lim= 20
+
+black = 0
+grey  = 100
+light_grey = 200
+white =256
+
+print('stencil config hi_lim = {}, load = {}, 3 stencil'.format(hi_lim,lo_lim))
+#stencil A dark cols
+a_img = e_img.copy()
+a_pixelsNew = a_img.load()
+import numpy as np
+for i in range(a_img.size[0]):
+    for j in range(a_img.size[1]):
+     #   print(pixelsNew[i,j])
+        if a_pixelsNew[i,j] <= lo_lim:
+            a_pixelsNew[i,j] = black
+        else:
+            a_pixelsNew[i,j] = white
+
+a_img.save('a_stencil_darks.png')
+
+#stencil b mid cols
+b_img = e_img.copy()
+b_pixelsNew = b_img.load()
+import numpy as np
+for i in range(b_img.size[0]):
+    for j in range(b_img.size[1]):
+        #print(pixelsNew[i,j])
+        if lo_lim <= b_pixelsNew[i,j] < hi_lim:
+            b_pixelsNew[i,j] = black
+        else:
+            b_pixelsNew[i,j] = white
+
+b_img.save('b_stencil_mids.png')
+
+#stencil c light cols
+c_img = e_img.copy()
+c_pixelsNew = c_img.load()
+import numpy as np
+for i in range(c_img.size[0]):
+    for j in range(c_img.size[1]):
+        #print(pixelsNew[i,j])
+        if c_pixelsNew[i,j] >= hi_lim:
+            c_pixelsNew[i,j] = black
+        else:
+            c_pixelsNew[i,j] = white
+
+c_img.save('c_stencil_lights.png')
